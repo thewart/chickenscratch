9f93ce3102f23cc0e0f73a2376f49042f655e1a6
Seth Madlon-Kay <seth.madlonkay@gmail.com>
2019-01-09 21:29:14 -0500

    work in progress...

--- runmodel.R
+++ runmodel.R
@@ -8,6 +8,14 @@ standat$S <- length(standat$L)
 standat <- c(standat,list(QI=1,CI=1,KI=1,VI=1,GI=1,LI=1,OI=1))
 
 chmd <- stan_model("~/code/chickenscratch/omnichicken.stan")
-fit <- optimizing(chmd,standat,verbose=T,as_vector=F,refresh=100)
 
 stanobj <- sampling(chmd,standat,chains=0)
+
+log_prob()
+
+tbl <- fread("~/Downloads/Rani_tbl.csv")
+tbli <- tbl[1,-1] %>% as.vector()
+cpar <- list(alpha=tbli[1],tau=tbli[2],Qinit=tbli[3],beta_0=tbli[4:5],
+             beta_q=tbli[6:7],beta_k=tbli[8:9],beta_v=tbli[10:11],
+             init_0_opp=tbli[12],init_coh_opp=tbli[13],init_v_opp=tbli[14],init_vbycoh_opp[15],
+             eta=matrix(tbli[15:18]),Vo=tbli[20])
